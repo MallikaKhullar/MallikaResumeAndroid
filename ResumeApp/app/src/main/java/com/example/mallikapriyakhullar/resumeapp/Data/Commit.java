a84fb7c654703c5d50c0d568247936f12161ba83
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-08-12 03:40:47 +0530

    Creating model for commit

---
+++ ResumeApp/app/src/main/java/com/example/mallikapriyakhullar/resumeapp/Data/Commit.java
@@ -0,0 +1,33 @@
+package com.example.mallikapriyakhullar.resumeapp.Data;
+
+/**
+ * Created by mallikapriyakhullar on 12/08/17.
+ */
+
+public class Commit {
+    Committer committer;
+    String message;
+    String url;
+
+    public Committer getCommitter() {
+        return committer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Commit(Committer committer, String message, String url) {
+        this.committer = committer;
+        this.message = message;
+        this.url = url;
+    }
+
+    public Commit() {
+
+    }
+}
---
+++ ResumeApp/app/src/main/java/com/example/mallikapriyakhullar/resumeapp/Data/Committer.java
@@ -0,0 +1,44 @@
+package com.example.mallikapriyakhullar.resumeapp.Data;
+
+/**
+ * Created by mallikapriyakhullar on 12/08/17.
+ */
+
+public class Committer {
+
+    String login;
+    String imageUrl;
+    String profileUrl;
+
+    public Committer() {}
+
+    public Committer(String login, String imageUrl, String profileUrl) {
+        this.login = login;
+        this.imageUrl = imageUrl;
+        this.profileUrl = profileUrl;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+}
